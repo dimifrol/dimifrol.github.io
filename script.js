81f764c7ac42f13898283223ab00d17599580ee2
dimifrol <dimifrol@yandex.ru>
2016-12-11 17:14:11 +0300

    Обновили супрем

---
+++ script.js
@@ -0,0 +1,7 @@
+// 1. Выделить элемент. У элемента класс date.
+var date = document.querySelector('.date')
+// 2. Выбрали и сохранили элемент с классом date.
+
+consol.log(date);
+
+var date = $('.date');
