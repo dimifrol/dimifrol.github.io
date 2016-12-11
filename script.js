3239454122df5b6c19ec505c86954e02d9a9fa80
dimifrol <dimifrol@yandex.ru>
2016-12-11 19:29:17 +0300

    Скрипт времени

--- script.js
+++ script.js
@@ -1,7 +1,28 @@
 // 1. Выделить элемент. У элемента класс date.
-var date = document.querySelector('.date')
+
+var $logo = $('.logo');
+var $date = $('.date');
 // 2. Выбрали и сохранили элемент с классом date.
 
-consol.log(date);
+function getDate() {
+var realDate = new Date();
+//2. Получить дату
 
-var date = $('.date');
+var day = realDate.getDate();
+var month = realDate.getMonth()+1;
+var year = realDate.getFullYear();
+
+var hour = ('0' + realDate.getHours()).slice(-2);
+var minutes = ('0' + realDate.getMinutes()).slice(-2);
+var seconds = ('0' + realDate.getSeconds()).slice(-2);
+
+var dateHtml = day + '/' + month + '/' + year + ' ' + hour + ':' + minutes + ':' + seconds + ' ' + 'MSC';
+
+$date.text(dateHtml);
+}
+
+$logo.click(getDate);
+
+//setTimeout(getDate, 1000);
+getDate();
+setInterval(getDate, 1000);
